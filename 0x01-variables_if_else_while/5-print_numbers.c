0531099b605dff2828e9b5e74fd488967691b502
DavidDlhz <vidda1959@gmail.com>
2020-02-06 23:48:41 +0000

    Fix againt the print numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,17 +1,16 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-int num;
-for (num = 0; num < 9; num++)
+int a;
+for (a = 0; a < 10; a++)
 {
-printf("%i", num);
+printf("%i", a);
 }
 putchar('\n');
 return (0);
