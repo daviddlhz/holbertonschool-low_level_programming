f4ba5e3bc1e5e650b2d031d4b0ea247966f11672
DavidDlhz <vidda1959@gmail.com>
2020-02-11 22:14:47 +0000

    Firts the last

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 /**
  * print_to_98 - check the code for Holberton School students.
- *@n: this is the parameter number.
+ * @n: this is the parameter number.
  * Return: Always 0.
  */
 void print_to_98(int n)
