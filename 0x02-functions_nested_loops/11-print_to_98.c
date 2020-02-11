369763aba732c28737f771e925d4d2203c2e0253
DavidDlhz <vidda1959@gmail.com>
2020-02-11 22:59:39 +0000

    okkkk sooooo

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,35 +1,22 @@
 #include "holberton.h"
 #include <stdio.h>
+
 /**
- * print_to_98 - check the code for Holberton School students.
- * @n: this is the parameter number.
+ * print_to_98 - prints from any integer to 98
+ * @n: integer input
  *
- * Return: void.
+ * Return: void character
  */
+
 void print_to_98(int n)
 {
-	int a;
-	int b;
-
-	if (n < 98)
+	while (n != 98)
 	{
-		for (a = n; a < 98; a++)
-		{
-			if (a != 98)
-				printf("%d, ", a);
-			else if (a == 98)
-				printf("%d\n", a);
-		}
-
+		printf("%i, ", n);
+		if (n > 98)
+			n--;
+		else
+			n++;
 	}
-	else if (n > 98)
-	{
-		for (b = n; b >= 98; b--)
-		{
-			if (b != 98)
-				printf("%d, ", b);
-			else if (b == 98)
-				printf("%d\n", b);
-		}
-	}
+	printf("98\n");
 }
