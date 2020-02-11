aba06a62c36ae8b60256278d2f1e2bd8020380da
DavidDlhz <vidda1959@gmail.com>
2020-02-11 22:56:45 +0000

    Found?

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -3,7 +3,8 @@
 /**
  * print_to_98 - check the code for Holberton School students.
  * @n: this is the parameter number.
- * Return: Always 0.
+ *
+ * Return: void.
  */
 void print_to_98(int n)
 {
