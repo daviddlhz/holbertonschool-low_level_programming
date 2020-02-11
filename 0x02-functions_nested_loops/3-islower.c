4f7c14e59528ace6fe4837740c75ad541989d8bd
DavidDlhz <vidda1959@gmail.com>
2020-02-11 16:16:59 +0000

    This function return 1 is the letter is lowercase else 0

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,10 @@
+#include "holberton.h"
+/**
+ * _islower - check the code for Holberton School students.
+ * @c: is the parameter.
+ * Return: Always 0.
+ */
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
