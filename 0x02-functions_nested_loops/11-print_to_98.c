8f2b0558a9c9472a1f13ba6960909197a214fca7
DavidDlhz <vidda1959@gmail.com>
2020-02-11 22:22:25 +0000

    Fix the last task in the 0x02

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+#include <stdio.h>
 /**
  * print_to_98 - check the code for Holberton School students.
  * @n: this is the parameter number.
@@ -8,7 +9,7 @@ void print_to_98(int n)
 {
 	while (n != 98)
 	{
-		printf("%i, ", n);
+		printf("%i ,", n);
 		if (n > 98)
 			n--;
 		else
