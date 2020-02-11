884598cec07ffd92948714c32b4ca91a8b67b2e4
DavidDlhz <vidda1959@gmail.com>
2020-02-11 21:18:45 +0000

    This function add two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * add - check the code for Holberton School students.
+ *@a: is the firts parameter.
+ *@b: is the second parameter.
+ * Return: Always 0.
+ */
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+
+	return (c);
+
+}
