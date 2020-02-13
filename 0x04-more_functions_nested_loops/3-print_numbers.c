5bde67e9fd5eb3ff4d4b96dfc78159ff63acbf3f
DavidDlhz <vidda1959@gmail.com>
2020-02-13 17:33:47 +0000

    This function printf the 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * print_numbers - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+void print_numbers(void)
+{
+	for (int i = '0'; i <= '9'; i++)
+	{
+	_putchar(i);
+	}
+}
