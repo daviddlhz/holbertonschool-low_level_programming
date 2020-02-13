f376974bde9a8a83716c30354d4b4927ce76231d
DavidDlhz <vidda1959@gmail.com>
2020-02-13 20:17:33 +0000

    This function print straight  lines

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * print_line - check the code for Holberton School students.
+ * @n: is the parameter.
+ * Return: Always 0.
+ */
+void print_line(int n)
+{
+	int i;
+
+	i = 0;
+
+	while (i <= n)
+	{
+		_putchar('_');
+		i++;
+	}
+}
