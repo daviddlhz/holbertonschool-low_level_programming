294eada4c554d92f14abf0cafeac5fb0cb3a82ff
DavidDlhz <vidda1959@gmail.com>
2020-02-13 17:43:45 +0000

    Fix the error

--- 0x04-more_functions_nested_loops/main.c
+++ 0x04-more_functions_nested_loops/main.c
@@ -1,6 +1,4 @@
 #include "holberton.h"
-#include <stdio.h>
-
 /**
  * main - check the code for Holberton School students.
  *
@@ -8,11 +6,6 @@
  */
 int main(void)
 {
-	char c;
-
-	c = '0';
-	printf("%c: %d\n", c, _isdigit(c));
-	c = 'a';
-	printf("%c: %d\n", c, _isdigit(c));
+	print_numbers();
 	return (0);
 }
