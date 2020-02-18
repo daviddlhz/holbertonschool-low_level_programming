80906ddeb42165fafe4eee52051038cf7c4958c7
DavidDlhz <vidda1959@gmail.com>
2020-02-18 20:53:35 +0000

    This function prints one string in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ * print_rev - check the code for Holberton School students.
+ * @str: is the parameter.
+ */
+void print_rev(char *str)
+{
+	int i;
+	int j;
+
+	for (i = 0; str[i] != 0; i++)
+	{
+	}
+
+	for (j = i; j >= 0; j--)
+	{
+		_putchar(str[j]);
+	}
+	_putchar('\n');
+}
