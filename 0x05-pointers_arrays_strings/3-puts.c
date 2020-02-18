7bb524a8da1020ace1831278e1c9b7e428533908
DavidDlhz <vidda1959@gmail.com>
2020-02-18 19:43:47 +0000

    This function prints a string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * _puts - check the code for Holberton School students.
+ * @str: is the parameter.
+ * Return: Always 0.
+ */
+void _puts(char *str)
+{
+	int i, len;
+	len = strlen(str);
+
+	for (i = 0; i <= len; i++)
+	{
+		_putchar(str[i]);
+	}
+}
--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -1,8 +1,12 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 
+#include <stdio.h>
+#include <string.h>
+
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
+void _puts(char *str);
 
 #endif /* HOLBERTON_H */
