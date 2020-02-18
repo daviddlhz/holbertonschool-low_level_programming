2af5f10343c742031b550ab2ccb05fa4ac084df9
DavidDlhz <vidda1959@gmail.com>
2020-02-18 15:24:40 +0000

    This function change the value in variable n with pointers

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "holberton.h"
+/**
+ * 0-reset_to_98.c - check the code for Holberton School students.
+ * @n: is the parameter.
+ * Return: Always 0.
+ */
+int reset_to_98(int *n)
+{
+	*n = 98;
+}
