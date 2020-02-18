8c5ee5531b3d8b7d227b82070ed78a18a0c16c94
DavidDlhz <vidda1959@gmail.com>
2020-02-18 22:00:30 +0000

    This function write in reverse

--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -1,6 +1,7 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 
+#include<stdio.h>
 
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
@@ -8,5 +9,6 @@ int _strlen(char *s);
 void _puts(char *str);
 int _putchar(char c);
 void print_rev(char *s);
+void rev_string(char *s);
 
 #endif /* HOLBERTON_H */
