24c5f359d5260ef0c76fc5f896b72616beb19531
David De La Hoz <vidda1959@gmail.com>
2020-02-24 20:51:10 -0500

    This function sum the matrix in diagonals

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * print_diagsums - check the code for Holberton School students.
+ * @a: is the parameter.
+ * @size: is other parameter.
+ * Return: Always 0.
+ */
+void print_diagsums(int *a, int size)
+{
+	int i, j, p, l = 0, r = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		p = (i * size) + i;
+		l += *(a + p);
+	}
+	for (j = 0; j < size; j++)
+	{
+		p = (j * size) + (size - 1 - j);
+		r += *(a + p);
+	}
+	printf("%i, %i\n", l, r);
+}
