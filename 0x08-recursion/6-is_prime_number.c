e7f2591261be8e7afe756d361ed372cb1c15174e
David De La Hoz <vidda1959@gmail.com>
2020-02-26 22:14:07 -0500

    Fix the task 6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,38 +1,60 @@
 #include "holberton.h"
 
+int prime_w(int n, int i);
+
 /**
- * is_prime_number - returns 1 if the input integer is a prime number,
- * otherwise return 0.
- * @n: int type Integer
- * Return: primeNum.
+ *is_prime_number - say if is prime or not
+ *@n: number to be checked
+ *Return: return 1 if primer 0 otherwise
  */
 
 int is_prime_number(int n)
 {
+
+	int i;
+
+	i = 2;
+
+	/* 0,1 not considered prime numbers*/
 	if (n < 2)
 	{
 		return (0);
 	}
 
-	return (primeNum(n, n - 1));
-	}
-
-/**
- * primeNum - Count Prime Numbers
- * @n: int type Integer
- * @a: int type Integer
- * Return: primeNum
- */
-
-int primeNum(int n, int a)
-{
-	if (a == 1)
+	if (n == 2)
 	{
 		return (1);
 	}
-	if (n % a == 0)
+
+	return (prime_w(n, i));
+
+}
+
+/**
+ *prime_w - recursion loop
+ *@n: number to be considered
+ *@i: number to iterate
+ *Return: return 1or 0
+ */
+
+int prime_w(int n, int i)
+{
+
+	if (i == n - 1)
+	{
+		return (1);
+	}
+
+	if (n % i  == 0)
 	{
 		return (0);
 	}
-	return (primeNum(n, a - 1));
+
+	if (n % i  != 0)
+	{
+		return (prime_w(n, i + 1));
+	}
+
+	return (0);
+
 }
