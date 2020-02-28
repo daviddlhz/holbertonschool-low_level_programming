85877597d9993bc78a929a0b693629c73177b31c
DavidDlhz <vidda1959@gmail.com>
2020-02-28 23:06:25 +0000

    Pushing

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * main - this function prints the name program
+ * @argv: return of size of argv.
+ * @argc: return of values of program.
+ * Return: Nothing is recursive.
+ */
+int main(__attribute__((unused)) int argc, char *argv[])
+{
+	printf("%s \n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * main - this function prints the lenght of parameters of the programan.
+ * @argv: return of size of argv.
+ * @argc: return of values of program.
+ * Return: 0 Always.
+ */
+int main(__attribute__((unused)) int argc, char *argv[])
+{
+	printf("%i \n", argc);
+	return (0);
+}
