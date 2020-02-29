72c15e42dd421dba9a86f1dcdaed84067944aeca
DavidDlhz <vidda1959@gmail.com>
2020-02-29 00:22:22 +0000

    This function or program prints all arguments it receives

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ * main - this function prints all arguments it recives.
+ * @argv: return of size of argv.
+ * @argc: return of values of program.
+ * Return: 0 Always.
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	i = 0;
+
+	while ( i < argc)
+	{
+		printf("%s \n", argv[i]);
+		i++;
+	}
+
+	return (0);
+}
