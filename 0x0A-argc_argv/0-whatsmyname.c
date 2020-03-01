09509745b6ea7842531e782faac7ab02ea421e11
David De La Hoz <vidda1959@gmail.com>
2020-02-29 23:46:11 -0500

    Fix??

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,12 +1,15 @@
+#include "holberton.h"
 #include <stdio.h>
+
 /**
  * main - this function prints the name program
- * @argv: return of size of argv.
- * @argc: return of values of program.
- * Return: Nothing is recursive.
+ * @argv: return of size of argv
+ * @argc: return of values of program
+ * Return: 0
  */
 int main(__attribute__((unused)) int argc, char *argv[])
 {
-	printf("%s \n", argv[0]);
+	printf("%s\n", argv[0]);
+	
 	return (0);
 }
