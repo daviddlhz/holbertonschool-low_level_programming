4fe2a4bc5001ab97ad95d9e3a080fd2005de1e8f
David De La Hoz <vidda1959@gmail.com>
2020-02-29 23:55:06 -0500

    Fix the error

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,13 +1,13 @@
 #include "holberton.h"
 #include <stdio.h>
 /**
- * main - this function prints the lenght of parameters of the programan.
- * @argv: return of size of argv.
- * @argc: return of values of program.
- * Return: 0 Always.
+ * main - this function prints the lenght of parameters of the programan
+ * @argv: return of size of argv
+ * @argc: return of values of program
+ * Return: 0 Always
  */
 int main(int argc, __attribute__((unused)) char *argv[])
 {
-	printf("%i \n", (argc - 1));
+	printf("%d\n", argc - 1);
 	return (0);
 }
