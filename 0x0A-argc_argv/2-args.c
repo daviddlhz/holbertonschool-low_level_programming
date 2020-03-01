abd92d21950941c6435902b4d46da411a6982c5b
David De La Hoz <vidda1959@gmail.com>
2020-03-01 00:10:26 -0500

    Fix the error in 2

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,9 +1,10 @@
 #include "holberton.h"
+#include <stdio.h>
 /**
- * main - this function prints all arguments it recives.
- * @argv: return of size of argv.
- * @argc: return of values of program.
- * Return: 0 Always.
+ * main - this function prints all arguments it recives
+ * @argv: return of size of argv
+ * @argc: return of values of program
+ * Return: 0 Always
  */
 int main(int argc, char *argv[])
 {
@@ -11,7 +12,7 @@ int main(int argc, char *argv[])
 
 	i = 0;
 
-	while ( i < argc)
+	while (i < argc)
 	{
 		printf("%s \n", argv[i]);
 		i++;
