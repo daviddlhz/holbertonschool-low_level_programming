4d109d9e89413bb39bc5ad92e5832cbca6fb4d65
DavidDlhz <vidda1959@gmail.com>
2020-03-03 19:05:30 +0000

    Fix the error

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,10 +1,12 @@
 #include "holberton.h"
+
 /**
  * create_array - is a function using malloc
  * @size: is a size of array
  * @c: is a content that save in the array
  * Return: null is size is 0 if not return a pointer.
  */
+
 char *create_array(unsigned int size, char c)
 {
 	unsigned int i;
@@ -21,6 +23,7 @@ char *create_array(unsigned int size, char c)
 			p[i] = c;
 			i++;
 		}
+		p[i] = '\0';
 
 		return (p);
 	}
