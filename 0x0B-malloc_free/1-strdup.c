fe3017596d8b7f42d18f7c572db40640ee6e9714
DavidDlhz <vidda1959@gmail.com>
2020-03-03 22:28:35 +0000

    commit

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -9,39 +9,4 @@ char *_strdup(char *str)
 {
 	int i;
 	int j;
-	char *p;
-
-	i = 0;
-
-	if (str != NULL)
-	{
-
-	while (str[i] != '\0')
-	{
-		i++;
-	}
-	}
-	else
-	{
-		return (NULL);
-	}
-
-	p = malloc(i * sizeof(char));
-
-	if (p != NULL)
-	{
-		for (j = 0; j < i; j++)
-		{
-
-		p[j] = str[j];
-
-		}
-
-		return (p);
-	}
-	else
-	{
-
-		return (NULL);
-	}
-}
+	char 
---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,40 @@
+#include "holberton.h"
+/**
+ *str_concat - returns a pointer that contains a string
+ * that is a combination of two strings
+ * @s1: string 1
+ * @s2: string 2
+ * Return: pointer to the concatenated string
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	char *str;
+	char *emp;
+	int i, j, k;
+
+	emp = "";
+	if (s1 == NULL)
+		s1 = emp;
+	if (s2 == NULL)
+		s2 = emp;
+	for (i = 0; s1[i] != '\0'; i++)
+		;
+	for (j = 0; s2[j] != '\0'; j++)
+		;
+	j++;
+	str = malloc((i + j) * sizeof(*str));
+	if (str == NULL)
+		return (NULL);
+	for (k = 0; s1[k] != '\0'; k++)
+		str[k] = s1[k];
+	j = 0;
+	while (s2[j] != '\0')
+	{
+		str[k] = s2[j];
+		j++;
+		k++;
+	}
+	str[k] = '\0';
+	return (str);
+}
