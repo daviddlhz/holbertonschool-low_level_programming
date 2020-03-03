c603e3c5cd59122d89e4699f9e0e23b3556b45f3
DavidDlhz <vidda1959@gmail.com>
2020-03-03 22:39:30 +0000

    Found

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * free_grid - function to free memory from grid
+ * @grid: grid to free;
+ * @height: height of grid;
+ * Return: none
+ */
+
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+		free(grid[i]);
+	free(grid);
+}>)}
