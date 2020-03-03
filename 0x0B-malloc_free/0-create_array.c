308e9f43be868e1b1af3bb64f1479e005093c17d
DavidDlhz <vidda1959@gmail.com>
2020-03-03 18:57:36 +0000

    This function create a array with chars using malloc

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+/**
+ * create_array - is a function using malloc
+ * @size: is a size of array
+ * @c: is a content that save in the array
+ * Return: null is size is 0 if not return a pointer.
+ */
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *p;
+
+	i = 0;
+
+	if (size != 0)
+	{
+		p = malloc(size * sizeof(char));
+
+		while (i != (size + 1))
+		{
+			p[i] = c;
+			i++;
+		}
+
+		return (p);
+	}
+	else
+	{
+		return (NULL);
+	}
+}
