5d99c4becbc3a035f2ba93b84f592d939adfacf2
David De La Hoz <vidda1959@gmail.com>
2020-03-03 21:38:40 -0500

    Fix

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,54 +1,40 @@
-#include <string.h>
+#include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
+
 /**
- * main - prints the minimum number of coins to make change
- * for an amount of money
- * @argc: arguement count
- * @argv: argument numbers
- * Return: 0;
+ * main - prints the minimum number of coins
+ * for a specific amount of change
+ * @argc: number of arguements
+ * @argv: array of arguements
+ * Return: 0 for success, for error
  */
-
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-	int value, count;
+	int num, i, res;
+	int coins[] = {25, 10, 5, 2, 1};
 
-	count = 0;
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	value = atoi(argv[1]);
-	if (value < 0)
+	num = atoi(argv[1]);
+	res = 0;
+
+	if (num < 0)
 	{
-		printf("%d\n", 0);
+		printf("0\n");
 		return (0);
 	}
-	if (value % 25 >= 0)
+
+	for (i = 0; i < 5 && num >= 0; i++)
 	{
-		count += value / 25;
-		value = value % 25;
+		while (num >= coins[i])
+		{
+			res++;
+			num -= coins[i];
+		}
 	}
-	if (value % 10 >= 0)
-	{
-		count += value / 10;
-		value = value % 10;
-	}
-	if (value % 5 >= 0)
-	{
-		count += value / 5;
-		value = value % 5;
-	}
-	if (value % 2 >= 0)
-	{
-		count += value / 2;
-		value = value % 2;
-	}
-	if (value % 1 >= 0)
-	{
-		count += value / 1;
-	}
-	printf("%d\n", count);
+	printf("%d\n", res);
 	return (0);
 }
