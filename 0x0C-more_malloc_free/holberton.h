f7fae25d1c9d9b159f8f12a964623e2533d3d962
David De La Hoz <vidda1959@gmail.com>
2020-03-05 20:53:26 -0500

    Sooo?

--- 0x0C-more_malloc_free/holberton.h
+++ 0x0C-more_malloc_free/holberton.h
@@ -5,11 +5,6 @@
 #include <stdlib.h>
 
 int _putchar(char c);
-char *create_array(unsigned int size, char c);
-char *_strdup(char *str);
-char *str_concat(char *s1, char *s2);
-int **alloc_grid(int width, int height);
-char *argstostr(int ac, char **av);
-
+void *malloc_checked(unsigned int b);
 
 #endif /* HOLBERTON_H */
