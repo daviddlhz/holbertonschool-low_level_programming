7a8bb9d6a722c24f99309b0d954f06147a698437
daviddlhz <vidda1959@gmail.com>
2020-03-15 15:01:01 -0500

    last task

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -4,10 +4,18 @@
 #include <stdio.h>
 #include <stdarg.h>
 
+typedef struct print
+{
+	char *opc;
+	void (*f)();
+} opc_t;
+
 int sum_them_all(const unsigned int n, ...);
 
 void print_numbers(const char *separator, const unsigned int n, ...);
 
 void print_strings(const char *separator, const unsigned int n, ...);
+
+void print_all(const char * const format, ...);
 
 #endif
