1c8a37eccaf54626d9436a69ab630a12e2470776
daviddlhz <vidda1959@gmail.com>
2020-03-26 22:55:31 -0500

    Betty fix task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,28 +8,23 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int num;
-	unsigned int pow;
+	unsigned int num, pow;
 	int i;
 
 	if (b == NULL)
 	{
 		return (0);
-
 	}
-
 	i = 0;
+
 	while (b[i])
 	{
 		if (b[i] != '0' && b[i] != '1')
 		{
 			return (0);
-
 		}
 		i++;
-
 	}
-
 	num = 0;
 	pow = 1;
 	i--;
@@ -39,14 +34,9 @@ unsigned int binary_to_uint(const char *b)
 		if (b[i] == '1')
 		{
 			num = num + pow;
-
 		}
-
 		i--;
 		pow = pow * 2;
-
 	}
-
 	return (num);
-
 }
