dcc385dc1be8c7329ad2d030277d7d2a8ded6594
daviddlhz <vidda1959@gmail.com>
2020-04-19 15:00:35 -0500

    Print every number positive or negative

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+ * print_number - prints numbers positive o negatives.
+ *@n: integer
+ * Return: void
+ */
+void print_number(int n)
+{
+	unsigned int a;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		a = -n;
+	}
+	else
+	{
+		a = n;
+	}
+	if (a / 10)
+	{
+		print_number(a / 10);
+	}
+	_putchar(a % 10 + '0');
+}
