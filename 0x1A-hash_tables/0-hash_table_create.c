104b6702b2043493ab30fc809647a612ad6e3cfc
daviddlhz <vidda1959@gmail.com>
2020-05-29 12:31:47 -0500

    Creates hash Tables

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -6,5 +6,29 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	
+	hash_table_t *ht;
+
+	if (size < 1)
+	{
+		return (NULL);
+	}
+
+	ht = malloc(sizeof(hash_table_t));
+
+	if (ht == NULL)
+	{
+		return (NULL);
+	}
+
+	ht->size = size;
+
+	ht->array = malloc(sizeof(hash_node_t));
+
+	if (ht->array == NULL)
+	{
+		free(ht);
+		return (NULL);
+	}
+
+	return (ht);
 }
