0aed2dc2141cfb5f8f97ec5e553ff67270b2bfc3
daviddlhz <vidda1959@gmail.com>
2020-05-29 00:04:51 -0500

    Init commit

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,10 @@
+#include "hash_tables.h"
+/**
+ * hash_table_create - prints any string in Reverse.
+ * @size: is the size of the array
+ * Return: Hash table.
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	
+}
---
+++ 0x1A-hash_tables/README.md
@@ -0,0 +1,1 @@
+__Hash tables in C__
